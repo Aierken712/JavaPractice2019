39fd5a5d82dbd37d98976b0e412f745090815bd0
Aierken712 <ershad0419@gmail.com>
2020-02-29 18:21:50 -0500

    update

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -5,5 +5,6 @@ public class StringPractice {
     public static void main(String[] args) {
         System.out.println("Hello, world");
         System.out.println("www");
+        System.out.println("qqq");
     }
 }
